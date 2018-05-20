9241ac6ff27491ce524ae0f92292f69f28becc50
JAreina <jareinafdez@gmail.com>
2018-05-20 18:38:29 +0200

    velocidad declarativa

---
+++ 2-imperativo-o-declarativo/2-imperativoDeclarativo.js
@@ -0,0 +1,30 @@
+const numeros = [1,2,3,4,5,6,7,8,9,10];
+
+// programacion imperativa
+
+console.time('IMPERATIVO')
+function hallarCuadrado(numeros){
+    let arr=[];
+
+   for (let index = 0; index < numeros.length; index++) {
+       arr.push(Math.pow(numeros[index],2));
+       
+   }
+
+   console.log(arr);
+
+}
+hallarCuadrado(numeros);
+console.timeEnd('IMPERATIVO')
+
+
+// PROGRAMACION DELCARATIVA
+
+console.time('DECLARATIVO')
+
+console.log(numeros.map( (num)=>{
+              return  Math.pow(num,2);
+                   }) 
+                 );
+
+console.timeEnd('DECLARATIVO');
